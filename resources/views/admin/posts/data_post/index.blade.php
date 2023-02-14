3b7f921e10e46d1ce1e45ebf9abebfe1c87733dd
PratamaR <nvl9502@gmail.com>
2023-02-14 16:17:52 +0700

    first commit

---
+++ resources/views/admin/posts/data_post/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin.app')
+@section('content')
+
+<div class="main-panel">
+    <div class="content">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">{{ $title }}</h4>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+@endsection
